41f46e34a8ed17e93eed20f43e156d2e56e3d882
Marmin <38385813+Marmin101@users.noreply.github.com>
2024-04-16 12:29:44 -0400

    Finish Telephone riddle
    Create random int and random string

--- Floors/Floor2/Telephone.cs
+++ Floors/Floor2/Telephone.cs
@@ -8,27 +8,44 @@ namespace NarrativeProject.Floors.Floor2
         internal override string CreateDescription() =>
 @"You hear the dial tone of the telephone.
 [Hang up]? (goes back to the corridor)
-";        
+";
+        internal static string whichDoor;
 
         internal override void ReceiveChoice(string choice)
         {
             if (choice == Poster.posterNum.ToString())
             {
                 Console.WriteLine("You enter the number you found on the poster. You hear the ringing tone.");
-                System.Threading.Thread.Sleep(1000);
-                Console.WriteLine("It rings. ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("It rings some more. ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("And more.");
-                System.Threading.Thread.Sleep(500);
-                Console.Write(".");
-                System.Threading.Thread.Sleep(500);
-                Console.Write(".");
                 System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("It rings. ");
+                System.Threading.Thread.Sleep(3000);
+                Console.Write("It rings some more. ");
+                System.Threading.Thread.Sleep(3000);
+                Console.Write("And more.");
+                System.Threading.Thread.Sleep(1500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(1500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(5000);
+                Console.WriteLine("");
                 Console.WriteLine("You hear a click and a muffled voice answers.");
-                Console.WriteLine('"' + "I've left you a gift." + '"');
-
+                System.Threading.Thread.Sleep(2750);
+                Console.WriteLine(" | v o i c e : " + '"' + "I've left you a gift." + '"');
+                System.Threading.Thread.Sleep(2000);
+                string[] choices = new string[] { "left", "right" };
+                Random rand = new Random();
+                int index = rand.Next(choices.Length);
+                whichDoor = choices[index];
+                Console.WriteLine(" | v o i c e : " + '"' + "The " + whichDoor + " door. It opens." + '"');
+                System.Threading.Thread.Sleep(1500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(1500);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(".");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("The phone line cuts.");
+                System.Threading.Thread.Sleep(2000);
             }
             else
             {
